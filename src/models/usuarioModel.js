bab1cde314ffd6e08b87343d27d32c57f276b669
PajorVi <vinicius.marques@sptech.school>
2024-06-08 16:50:51 -0300

    criando cadastro para uso da bobIA e estilizando interface

---
+++ src/models/usuarioModel.js
@@ -0,0 +1,25 @@
+var database = require("../database/config");
+
+function cadastrar(nome, email, senha) {
+    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():", nome, email, senha);
+    
+    var instrucaoSql = `
+        INSERT INTO usuarioSuporte (nome, email, senha) VALUES ('${nome}', '${email}', '${senha}');
+    `;
+    console.log("Executando a instrução SQL: \n" + instrucaoSql);
+    return database.executar(instrucaoSql);
+}
+
+function autenticar(email, senha) {
+    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function entrar(): ", email, senha)
+    var instrucaoSql = `
+        SELECT id, nome, email FROM usuarioSuporte WHERE email = '${email}' AND senha = '${senha}';
+    `;
+    console.log("Executando a instrução SQL: \n" + instrucaoSql);
+    return database.executar(instrucaoSql);
+}
+
+module.exports = {
+    cadastrar,
+    autenticar,
+}
